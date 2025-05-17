0932267f608b40c1024ad055b15a59d13acd7060
Ahmed-Salah-92 <ahmedsalah92.2025@gmail.com>
2025-05-17 18:49:38 +0300

    Implement interstitial ad loading and display in HomeActivity
    Signed-off-by: Ahmed-Salah-92 <ahmedsalah92.2025@gmail.com>

--- app/src/main/java/com/ragdoll/newsapp/HomeActivity.kt
+++ app/src/main/java/com/ragdoll/newsapp/HomeActivity.kt
@@ -2,16 +2,24 @@ package com.ragdoll.newsapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.ragdoll.newsapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-    var category = ""
+    private var category = ""
+    private var mInterstitialAd: InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +37,21 @@ class HomeActivity : AppCompatActivity() {
             // Optional: You can add any initialization code here if needed
         }
 
+        // Load an InterstitialAd
+        loadAdvertisement()
+
         // Set up the buttons
         binding.techBtn.setOnClickListener {
             category = "technology"
-            navigateToNewsActivity()
+            showAdvertisement()
         }
         binding.sportsBtn.setOnClickListener {
             category = "sports"
-            navigateToNewsActivity()
+            showAdvertisement()
         }
         binding.scienceBtn.setOnClickListener {
             category = "science"
-            navigateToNewsActivity()
+            showAdvertisement()
         }
 
         // Set up the toolbar
@@ -56,6 +67,53 @@ class HomeActivity : AppCompatActivity() {
             }
         }
 
+
+    }
+
+    private fun loadAdvertisement() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Replace with your actual ad unit ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d("TAG", adError.message.toString())
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d("TAG", "Ad was loaded.")
+                    mInterstitialAd = interstitialAd
+                }
+            }
+        )
+    }
+
+    private fun showAdvertisement() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    // Called when fullscreen content is dismissed.
+                    // Don't forget to set the ad reference to null so you
+                    // don't show the ad a second time.
+                    mInterstitialAd = null
+                    navigateToNewsActivity()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                    // Called when fullscreen content failed to show.
+                    // Don't forget to set the ad reference to null so you
+                    // don't show the ad a second time.
+                    mInterstitialAd = null
+                    navigateToNewsActivity()
+                }
+
+            }
+        } else {
+            navigateToNewsActivity()
+        }
 
     }
 
